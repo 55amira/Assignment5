0c7c029e5e24135f228cd7a1dbd0634043e290e4
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-24 01:11:57 +0200

    Nullable (Value , Reference)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -23,6 +23,53 @@
             //object O1 = 3;
             //int X =(int) O1; //unsafe 
             #endregion
+
+            //Nullable 
+            //Value Type
+
+            //int x = 6;
+            // x = null;
+
+            //int? Age = null; //Nullable<int>
+
+            //double? B = null;   //Nullable<double>
+
+            //int x = 5;
+
+            //int? y = x;
+            //int? x = 5;
+            //int y;
+            //y= x;//error 
+            //if (x != null)
+            //    y = (int)x;
+            //else
+            //    y = 0;
+            //if (x is not null)
+            //    y = (int)x;
+            //else
+            //    y = 0;
+            //if (x.HasValue)
+            //    y = x.Value;
+            //else
+            //    y = 0;
+
+            //y=x.HasValue? x.Value : 0;  
+            //y=x is not null ? x.Value: 0;
+            //y = x ?? 0;
+            //Console.WriteLine(y);
+
+
+            //Nullable Reference Type 
+
+            //string Message = null;
+            //string? Message2 = null;
+            //Console.WriteLine(Message);
+            //Console.WriteLine(Message2);
+
+
+
+
+
         }
     }
 }

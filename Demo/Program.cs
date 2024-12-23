fb4d8288fd08ac63b28b79bd38bbd1ed2b9824b5
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-24 00:50:11 +0200

    Boxing And UnBoxing

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Boxing And UnBoxing
+
+            ////Boxing 
+
+            //object O1;
+
+            //O1 = 1;
+            //O1 = "Ahmed";
+            //O1 = 3;
+
+            //O1 = 1.5;
+            //O1 = "a";
+            //O1 = true;
+            //O1 = new DateTime();     
+
+            //UnBoxing
+            //object O1 = 3;
+            //int X =(int) O1; //unsafe 
+            #endregion
         }
     }
 }

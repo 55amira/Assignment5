bae62c42cda78630bf9c5f2d2a5c4900c210edc0
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-24 08:18:25 +0200

    Q1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp5
+{
+    enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string day in Enum.GetNames(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
